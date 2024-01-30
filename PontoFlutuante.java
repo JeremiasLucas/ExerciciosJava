a647b157844f53abeff1eaf6d56547996c719e82
Jeremias Lucas <88001432+JeremiasLucas@users.noreply.github.com>
2024-01-30 11:45:35 -0300

    Add files via upload

---
+++ PontoFlutuante.java
@@ -0,0 +1,8 @@
+package variaveis;
+
+public class PontoFlutuante {
+	public static void main(String[] args) {
+		double hora = 3;
+		System.out.println("Agora são exatamente: " + hora + " horas");
+	}
+}

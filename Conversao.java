d7748110dc0672ce0234cd2b2a6414025d1bb4dc
Jeremias Lucas <88001432+JeremiasLucas@users.noreply.github.com>
2024-01-30 11:43:17 -0300

    Add files via upload

---
+++ Conversao.java
@@ -0,0 +1,18 @@
+package testeConvercao;
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		double salario = 2700.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		float pontoFlutuante = (float) 3.14;
+		
+		int valor2 = 20000000;
+		long numeroGrande = 328476521531623L;
+		short numeroPequeno = 2103;
+		byte b = 127;
+	}
+
+}
